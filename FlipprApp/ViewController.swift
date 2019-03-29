32106bff96791097f291d8a2969eea2c95169857
Chris Kuan <chris.kuan@hotmail.com>
2019-03-29 00:01:44 -0700

    Initial Commit

---
+++ FlipprApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipprApp
+//
+//  Created by Chris Kuan on 2019-03-29.
+//  Copyright © 2019 Chris Kuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
